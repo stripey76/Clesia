718bf695211c0c272a1f3fd8585a6a8f41f89787
stripey76 <92567126+stripey76@users.noreply.github.com>
2022-11-22 08:25:52 -0800

    cam pain

--- src/clesia/content/ClesiaSectors.java
+++ src/clesia/content/ClesiaSectors.java
@@ -14,30 +14,61 @@ public class ClesiaSectors {
 			addStartingItems = true;
 			captureWave = 10;
 			difficulty = 1;
-      overrideLaunchDefaults = true;
-      noLighting = true;
+                        overrideLaunchDefaults = true;
+                        noLighting = true;
 			startWaveTimeMultiplier = 3f;
 		}};
-		brokenOutpost = new SectorPreset("brokenOutpost", ClesiaPlanets.clesia, 35) {{
-			captureWave = 15;
+		wrangledForest = new SectorPreset("wrangledForest", ClesiaPlanets.clesia, 1) {{
+			captureWave = 12;
 			difficulty = 2;
-			startWaveTimeMultiplier = 2f;
 		}};
-
-		// lamoni
-		freshBeggining = new SectorPreset("FreshBeggining", ClesiaPlanets.clesia, 0) {{
-			alwaysUnlocked = true;
-			captureWave = 10;
-			difficulty = 1;
-			startWaveTimeMultiplier = 2f;
-		}};
-		reactiveMonolith = new SectorPreset("ReactiveMonolith", ClesiaPlanets.clesia, 54) {{
-			captureWave = 20;
-			difficulty = 1;
-			startWaveTimeMultiplier = 2f;
-		}};
-		sodaicOutpost = new SectorPreset("SodaicOutpost", ClesiaPlanets.clesia, 34) {{
+		farlandHeights = new SectorPreset("farlandHeights", ClesiaPlanets.clesia, 2) {{
 			difficulty = 3;
+		}};
+		crossways = new SectorPreset("crossways", ClesiaPlanets.clesia, 3) {{
+			captureWave = 20;
+			difficulty = 3;
+			startWaveTimeMultiplier = 3f;
+		}};
+		witheredRiver = new SectorPreset("witheredRiver", ClesiaPlanets.clesia, 4) {{
+			difficulty = 3;
+                        captureWave = 17;
+		}};
+                coulee = new SectorPreset("coulee", ClesiaPlanets.clesia, 5) {{
+			difficulty = 7;
+                        captureWave = 30;
+		}};
+                taintedNexus = new SectorPreset("taintedNexus", ClesiaPlanets.clesia, 12) {{
+			difficulty = 5;
+                        captureWave = 8;
+		}};
+                ravengedAxis = new SectorPreset("ravengedAxis", ClesiaPlanets.clesia, 15) {{
+			difficulty = 12;
+		}};
+                vacantQuadrant = new SectorPreset("vacantQuadrant", ClesiaPlanets.clesia, 35) {{
+			difficulty = 15;
+                        captureWave = 45;
+		}};
+                desertFlats = new SectorPreset("desertFlats", ClesiaPlanets.clesia, 74) {{
+			difficulty = 12;
+		}};
+                coldCrater = new SectorPreset("coldCrater", ClesiaPlanets.clesia, 45) {{
+			difficulty = 15;
+                        captureWave = 20;
+		}};
+                dreadCliff = new SectorPreset("dreadCliff", ClesiaPlanets.clesia, 46) {{
+			difficulty = 17;
+		}};
+                stranded = new SectorPreset("stranded", ClesiaPlanets.clesia, 50) {{
+			difficulty = 18;
+                        captureWave = 40;
+		}};
+                storageComplex = new SectorPreset("storageComplex", ClesiaPlanets.clesia, 51) {{
+			difficulty = 20;
+                        captureWave = 80;
+		}};
+                visionary = new SectorPreset("visionary", ClesiaPlanets.clesia, 79) {{
+			difficulty = 30;
 		}};
 	}
 }
